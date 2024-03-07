fea3de52a0d4d9b3c7181ccfe4b90057cbfab1f0
mahailahmed <160250995+mahailahmed@users.noreply.github.com>
2024-03-07 11:01:03 -0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,30 @@
+//                   topic     increment  
+
+
+//  ++increment
+
+
+                
+let x =10                                     //first method                  
+x =x +1
+console.log(x)
+
+
+
+                                    // second method
+let y=100
+// y= ++y                    // y ki value main 1+ karny k liye ++y krengy tu 1 add hujayga
+// console.log(y)
+
+
+// pre increment                          +1 add hujayga
+// ++y
+
+
+// post increment                                     jitni br console krengy utni dafa 1 add huga
+   y                       // 100
+console.log(y++)            // 101
+console.log(y++)             //102
+console.log(y++)              //103
+console.log(y++)             // 104
+console.log(y)
